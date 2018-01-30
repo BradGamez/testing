a2f9b6dcdbedee091b7be067e48fb6f9941a2547
BradGamez <33333609+BradGamez@users.noreply.github.com>
2018-01-29 21:34:21 -0500

    Add files via upload

---
+++ Testing.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const prefix = "-"
+
+bot.on('message', message => {
+    var guild = message.guild;
+    var author = message.author;
+    var args = message.content.split(' '); var g = " "; for(var i = 1; i < args.length; i++){ g = g+" "+args[i]; }
+    if (message.content.startsWith(prefix + 'setplayingstatus')) {
+        bot.user.setGame(g)
+        var embed = new Discord.RichEmbed();
+        if (message.author.bot) return;
+        embed.setColor('BLUE');
+        embed.setDescription("Playing status set to " + g);
+      message.channel.send({embed});
+    }
+});
+
+
+bot.login(process.env.BOT_TOKEN)
