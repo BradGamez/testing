bc09b58119562a3364fa0b220527677be6e657ff
BradGamez <33333609+BradGamez@users.noreply.github.com>
2018-01-29 21:39:45 -0500

    Update Testing.js

--- Testing.js
+++ Testing.js
@@ -11,7 +11,7 @@ bot.on('message', message => {
         var embed = new Discord.RichEmbed();
         if (message.author.bot) return;
         embed.setColor('BLUE');
-        embed.setDescription("Playing status set to" + g);
+        embed.setDescription("Playing status set to__" + g + "__");
       message.channel.send({embed});
     }
 });
