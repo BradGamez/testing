b37fc7c45a6c67f4705ec514fc25d08b30b91911
BradGamez <33333609+BradGamez@users.noreply.github.com>
2018-01-30 18:57:48 -0500

    Update Testing.js

--- Testing.js
+++ Testing.js
@@ -7,7 +7,7 @@ bot.on('message', message => {
     var author = message.author;
     var args = message.content.split(' '); var g = " "; for(var i = 1; i < args.length; i++){ g = g+" "+args[i]; }
     if (message.content.startsWith(prefix + 'setplayingstatus')) {
-    bot.user.setPresence({ game: { name: g, type: 1 } });        
+    bot.user.setPresence({ game: { name: g, type: 2 } });        
         var embed = new Discord.RichEmbed();
         if (message.author.bot) return;
         embed.setColor('BLUE');
